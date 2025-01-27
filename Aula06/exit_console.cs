65600e51ee244ad4c0be7d8a9d29841b159198de
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-27 17:30:11 -0300

    Commit caractere de escape - Aula05

---
+++ Aula06/exit_console.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula05{
+
+    static void Main(){
+
+        int n1, n2, n3;
+
+        n1 = 10; n2= 20; n3= 30;
+
+        Console.Write("\nn1=\t{0}\nn2=\t{1}\nn3=\t{2}",n1,n2,n3);
+// O \n é um caractere de escape. Serve para quebrar a linha gual ao Line do WriteLine.
+// O \t é um caracter de escape. Serve para fazer uma tabulação no código.
+    }
+}
