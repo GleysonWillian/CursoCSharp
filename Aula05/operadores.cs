1e2d9519fc56fb79bf43e1392ac781007e35cd0e
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-27 16:48:27 -0300

    Commit  Aula05

---
+++ Aula05/operadores.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Aula05{
+
+    static void Main(string[] args){
+
+        // categorias:
+
+        // atribuição (= e +):
+        //int  res = (10 + 5)*5;
+
+        //relacionais (true or false):
+        //bool res = 10!=5;
+        //operadores relacionais: >, <, <=, >=, !=.
+
+        //incremento e decremento:
+        int num = 0;
+
+        while (num < 20){
+            num ++;
+            Console.WriteLine($"Valor de num: {num}");
+        }
+
+        Console.WriteLine("Fim do programa!");
+
+    }
+}
---
+++ Aula05/teste.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 0; // Inicializa a variável num com valor 0
+
+        while (num < 20) // Condição: enquanto num for menor que 20
+        {
+            num++; // Incrementa o valor de num em 1
+            Console.WriteLine(num); // Exibe o valor atual de num
+        }
+
+        Console.WriteLine("Fim do programa!");
+    }
+}
