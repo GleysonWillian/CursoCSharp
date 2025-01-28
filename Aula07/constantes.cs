6922c56f2942fdc9052110f564c5110c0b093e3c
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-28 16:51:39 -0300

    Commit Aula 07 - Constantes

---
+++ Aula07/constantes.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Aula07{
+
+    static void Main(){
+
+        const string canal = "Gleyson Dev";
+        const double pi = 3.1514;
+
+        Console.WriteLine("Canal: {0}.\nPi: {1}.", canal, pi);
+
+    }
+}
