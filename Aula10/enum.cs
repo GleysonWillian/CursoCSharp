0b60f199f8a2aecbf2defd7425ced344f0128dde
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-28 18:33:44 -0300

    Commit Aula10 - enum

---
+++ Aula10/enum.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Aula10{
+
+    enum DiasSemana{Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado};
+
+    static void Main(){
+        // Com enum é um recurso que permite criar um tipo próprio com valores pré-definidos.
+
+            //cast: 
+            //DiasSemana ds = (DiasSemana)5;
+
+            //descobrindo o índice do dia através de conversão:
+            int ds = (int)DiasSemana.Sexta;
+
+            Console.WriteLine(ds);
+
+    }
+}
