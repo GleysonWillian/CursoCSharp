bfef44566bca185700d8063ec3ea00897e0c5cb1
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-28 17:27:32 -0300

    .

--- Aula08/formulario.cs
+++ Aula08/formulario.cs
@@ -9,14 +9,19 @@ class Aula08{
         string rua;
 
         Console.WriteLine("\n{10}Formulário de cadastrado:{10}\n");
+
         Console.WriteLine("Digite o seu nome: ");
-        nome = string(Console.ReadLine);
+        nome = string.Parse((Console.ReadLine));
+
         Console.WriteLine("Digite a sua idade: ");
         idade = int.Parse((Console.ReadLine));
+
         Console.WriteLine("Sexo (masc/fem): ");
-        nome = string(Console.ReadLine);
+        nome = string.Parse((Console.ReadLine));
+
         Console.WriteLine("Qual o nome da rua onde");
-        nome = string(Console.ReadLine);
+        nome = string.Parse((Console.ReadLine));
+        
         Console.WriteLine("Fim do programa!");
     }
 }
