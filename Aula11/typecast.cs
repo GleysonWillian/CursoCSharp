3688b4e05fe9124cbcfba8166eb09699b568797d
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-28 18:51:46 -0300

    Commit Aula11 - typecast

---
+++ Aula11/typecast.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Aula11{
+
+    static void Main(){
+
+        //operações taypast:
+
+        //Convertendo float em int:
+
+        //float n1 = 10.5f;
+        //int n2 = (int)n1;
+
+        //convertendo int em short:
+        int vInt = 10;
+        short vShort = (short)vInt;  
+
+        Console.WriteLine(vShort);
+
+    }
+}
+
+        // Conversão de um tipo para outro, quando não há uma conversão implícita//
