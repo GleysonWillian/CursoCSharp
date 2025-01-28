14549fb8535a126ac08d4b081369136184a81863
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-28 17:15:00 -0300

    Commit Aula08 - input

---
+++ Aula08/input.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aula08{
+
+    static void Main(){
+        int v1, v2, soma;
+        string nome;
+
+        Console.Write("Digite o seu nome: ");
+        nome = Console.ReadLine();
+        Console.WriteLine("Nome digitado: {0}", nome);
+
+    }
+}
---
+++ Aula08/teste_calculadora.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Aula08{
+
+    static void Main(){
+        int v1, v2, soma;
+        string nome;
+
+        Console.WriteLineriteLine("Digite o primeiro valor: ");
+        v1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo valor: ");
+        v2 = int.Parse(Console.ReadLine());
+        soma = v1 + v2;
+
+        Console.WriteLineriteLine("A soma de {0} e {1} é igual a {2}.",v1,v2,soma);
+        Console.WriteriteLine("Fim do programa!");
+    }
+}
