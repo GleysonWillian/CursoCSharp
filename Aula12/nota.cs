030e6e46f56a2c2dd7c1ee94cc92186423e17e06
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-29 15:35:54 -0300

    Commit de correção + Aula 13

--- Aula12/nota.cs
+++ Aula12/nota.cs
@@ -5,14 +5,14 @@ class Aula12{
     static void Main(){
 
         int nota = 0;
-        string resutado = "Reprovado";
+        string resultado = "Reprovado";
 
         Console.WriteLine("Digite o valor da nota: ");
         nota = int.Parse(Console.ReadLine());
 
         if (nota >= 60){
-            resutado = "Aprovado";
+            resultado = "Aprovado";
         }
-        Console.WriteLine("Resultado: {0}!", resutado);
+        Console.WriteLine("Resultado: {0}!", resultado);
     }
 }
