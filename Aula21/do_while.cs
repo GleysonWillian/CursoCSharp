e87b6c9cc61abf2dd3522777825626bc12e298fa
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-01-31 18:04:25 -0300

    commit do while

---
+++ Aula21/do_while.cs
@@ -0,0 +1,22 @@
+﻿// Os comandos são executados ao menos 1x.
+// Primeiro executa os camandos e depois testa (diferente do while).
+using System;
+class Aula21
+{
+    static void Main()
+    {
+        string senha = "123";
+        string senhauser;
+        int tentativas = 0;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a senha: ");
+            senhauser = Console.ReadLine();
+            tentativas++;
+
+        } while (senha != senhauser);
+
+        Console.WriteLine("Senha correta. Tentativas: {0}.", tentativas);
+    }
+}
