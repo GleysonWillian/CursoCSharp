b4801b03584d49739cc86ed20313e4c468c3e0b9
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-02-03 18:43:18 -0300

    métodos por valor e referência

---
+++ Aula25/valor_e_ref.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+class Aula25
+{
+    static void Main()
+    {
+        int num = 10;
+        // passagem por referência
+        dobrar1(ref num); 
+        // passagem por valor:
+        //dobrar2(num);
+        //Console.WriteLine(num);
+    }
+
+    //Passagem por referência:
+    static void dobrar1(ref int valor)
+    {
+        valor *= 2;
+    }
+    //Passagem por valor:
+    //static void dobrar2( int valor)
+    //{
+        //valor *= 2;
+}
