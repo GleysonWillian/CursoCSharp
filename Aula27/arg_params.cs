9b897f8dffd1bf4a6056f5e7c507e5b1d676f1e7
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-02-03 19:14:26 -0300

    argumentos params

---
+++ Aula27/arg_params.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Aula27
+{
+    static void Main()
+    {
+        soma(6,5,2,8,6,7);
+    }
+    static void soma(params int[] n)
+    {
+        int res = 0;
+
+        if (n.Length < 1)
+        {
+            Console.WriteLine("Não existem valores a serem somados.");
+        }
+        else if (n.Length < 2)
+        {
+            Console.WriteLine("Valores insuficientes para soma: {0}", n[0]);
+        }
+        else
+        {
+            for (int i = 0; i < n.Length; i++)
+            {
+                res += n[i];
+            }
+            Console.WriteLine("A soma dos valores é: {0}", res);
+        }
+    }
+}
